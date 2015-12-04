027daea3721b6e0138c7b711ce51f097c6be7793
Fabian K <grafska@gmail.com>
2015-12-04 15:22:00 +0100

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root :to => "visitors#index"
+
+  get '/tuitions', to: 'tuitions#index'
+  get '/tuitions/add', to: 'tuitions#add'
+  
   resources :students do
     get :subjects
   end
+  
+  resources :teachers
+  get '/teachers/:id/subjects', to: 'teachers#subjects', as: 'teacher_subjects'
+  get '/reports/subjects' ,to: 'reports#subjects', as: 'report_subjects'
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,14 +11,14 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
-    teacher: teachers.sample
+    teacher_id: teachers.sample.id
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
@@ -34,8 +34,8 @@ SubjectItem.all.each do |subject_item|
   subject_item.students.each do |student|
     rand(1..5).times do
       subject_item.subject_item_notes << SubjectItemNote.create(student: student,
-                                                                subject_item: subject_item,
-                                                                value: rand(1..6))
+                                                    subject_item_id: subject_item,
+                                                                value: rand(1..6))            
     end
   end
 end
